0bc9c1f7bc65be9a93e60b8475a63f3a10040db3
codename065 <codename065@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-21 15:01:05 +0000

    css issue
    git-svn-id: https://plugins.svn.wordpress.org/page-layout-builder/trunk@729421 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- minimax.php
+++ minimax.php
@@ -4,7 +4,7 @@ Plugin Name: Page Layout Builder
 Description: Drag and Drop Page Builder / Layout Builder / Content Builder for WordPress
 Plugin URI: http://wpeden.com
 Author: Shaon
-Version: 1.1.1
+Version: 1.1.2
 Author URI: http://wpeden.com
 */
  
@@ -46,29 +46,7 @@ function minimax_squeeze_page_canvas(){
          endif;
     }   
  
- function upload_logo(){
-        if(isset($_GET['task'])&&$_GET['task']=='uploadlogo'){
-           if(is_uploaded_file($_FILES['Filedata']['tmp_name'])){                                                                       /*File Uploaded function*/
-            move_uploaded_file($_FILES['Filedata']['tmp_name'],dirname(__FILE__).'/images/'.$_FILES['Filedata']['name']);
-            update_option("thm_logo",$_FILES['Filedata']['name']);
-            die("<img src='".get_bloginfo('template_url').'/images/'.get_option('thm_logo')."' />");   
-        }
-        die();    
-        }
-        
-    }
-                       
- function upload_favicon(){
-        if(isset($_GET['task'])&&$_GET['task']=='upload_favicon'){
-           if(is_uploaded_file($_FILES['Filedata']['tmp_name'])){                                                                       /*File Uploaded function*/
-            move_uploaded_file($_FILES['Filedata']['tmp_name'],dirname(__FILE__).'/images/'.$_FILES['Filedata']['name']);
-            update_option("thm_favicon",$_FILES['Filedata']['name']);
-            die("<img src='".get_bloginfo('template_url').'/images/'.get_option('thm_favicon')."' />");   
-        }
-        die();    
-        }
-        
-    }
+ 
     
  function minimax__module_status_change(){
      if(isset($_POST['module'])&&$_POST['module']){
@@ -92,36 +70,10 @@ function minimax_squeeze_page_canvas(){
          
  }
 
-
- function minimax_add_custom_box() {
-    add_meta_box( 'minimax_slider_link', __( 'Link', 'wp' ), 'minimax_slider_link', 'minimax_slider', 'side','core' );
-   
-}
-
-function minimax_slider_link( $post ) {
-    global $pt_plugin;  
-    ?>
-    <input type="text" size="46" name="minimax_slider_link" value="<?php echo get_post_meta($post->ID,"minimax_slider_link",true);?>">
-    <?php   
-    
-}
-
-function minimax_save_link( $post_id ) {
-     
-    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
-    if ( !current_user_can( 'edit_post', $post_id ) ) return;
-    if(!isset($_POST['minimax_slider_link'])) return;
-   
-    update_post_meta($post_id,'minimax_slider_link',$_POST['minimax_slider_link']);
-}
-
  
-add_action( 'add_meta_boxes', 'minimax_add_custom_box');
-add_action( 'save_post', 'minimax_save_link' ); 
+  
 
- 
-add_action( 'init', 'upload_logo', 0 );
-add_action( 'init', 'upload_favicon', 0 ); 
+  
 add_action( 'init', 'custom_init' ); 
  
 add_action('admin_menu', 'minimax_opt_menu');
