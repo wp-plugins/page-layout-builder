833d5c01fbeff98fdf1bf485fa704b2dbb10157d
codename065 <codename065@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-21 09:56:41 +0000

    version number update
    git-svn-id: https://plugins.svn.wordpress.org/page-layout-builder/trunk@743892 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- minimax.php
+++ minimax.php
@@ -4,7 +4,7 @@ Plugin Name: Page Layout Builder
 Description: Drag and Drop Page Builder / Layout Builder / Content Builder for WordPress
 Plugin URI: http://wpeden.com
 Author: Shaon
-Version: 1.2.0
+Version: 1.2.1
 Author URI: http://wpeden.com
 */
  
